3778e7879c926480dfb2bffb075102b333a9330d
Bastien Caudan <bastien.caudan@gmail.com>
2019-01-06 21:14:13 +0100

    remove CS

--- etc/env.sh
+++ etc/env.sh
@@ -4,35 +4,15 @@ CONF_DIR="$HOME/Library"
 BIN_DIR="/usr/local/bin"
 BREW_BIN_DIR="/usr/local/sbin"
 USER_BIN_DIR="$HOME/bin"
-CS_HOME="$HOME/Projets/ContentSquare"
 
 setopt rmstarsilent
 
 export FPATH="$FPATH:$USER_BIN_DIR"
 export PATH="$USER_BIN_DIR:$BIN_DIR:$BREW_BIN_DIR:$PATH:$OPT_DIR/ruby/bin"
 
-# virtualenv
-export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-
-# CS SSH
-export CS_USER="bastien"
-export AWS_USER="bastienc"
-
-# AWS SSH
-source "$CS_HOME/docs/setup-workstation/aws-ssh.sh"
-
 # Docker
 export LOCAL_IP=`ipconfig getifaddr en0 || ipconfig getifaddr en4`
 export DOCKER_HOST_IP=$LOCAL_IP
-
-# workspace
-export CS_ENV_DIR="$CS_HOME/cs-env"
-export LOCAL_WORKSPACE="$CS_HOME/workspace"
-export HUB_LOCAL_WORKSPACE="$CS_HOME/workspace/hub"
-export UXA_LOCAL_WORKSPACE="$CS_HOME/workspace/mousetest"
-export PUBLICITEST_LOCAL_WORKSPACE="$CS_HOME/workspace/publicitest"
-export PS_LOCAL_WORKSPACE="$CS_HOME/workspace/ps"
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
